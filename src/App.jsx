4897e5501028a24fb28428216529900f876743ba
Kubatbekkk <kubineccastro@gmail.com>
2023-06-10 16:34:42 +0600

    fix: react-router to react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,14 @@
-import { Alert, Box, Container } from '@mui/material';
-import GameTable from './features/GameTable';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import {
-  fetchQuestionByCategory,
-  selectIsAnswerCorrect,
-} from './features/categorySlice';
+import { Route, Routes } from 'react-router';
+import Game from './features/Game';
 
 function App() {
-  const status = useSelector((state) => state.status);
-  const error = useSelector((state) => state.error);
-  const isAnswerCorrect = useSelector(selectIsAnswerCorrect);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (status === 'idle') dispatch(fetchQuestionByCategory());
-  }, [status, dispatch]);
-
-  let content;
-  if (status === 'loading') {
-    content = <h1>Loading...</h1>;
-  } else if (status === 'succeeded') {
-    content = (
-      <>
-        <Container>
-          <Box
-            display="flex"
-            flexDirection="row"
-            gap="1rem"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <h1 className="heading__text">Jeopardy</h1>
-            {isAnswerCorrect !== null && (
-              <Box width="50%">
-                <Alert severity={`${isAnswerCorrect ? 'success' : 'error'}`}>
-                  {isAnswerCorrect
-                    ? 'The answer is correct'
-                    : 'The answer is incorrect'}
-                </Alert>
-              </Box>
-            )}
-          </Box>
-          <GameTable />
-        </Container>
-      </>
-    );
-  } else if (status === 'failed') {
-    content = <p>{error}</p>;
-  }
-
-  return <main>{content}</main>;
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/game" element={<Game />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
